6f778352eb3573de7840c49696cd77a5580fac60
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-08-23 19:18:06 +0300

    add auto route for app navigation

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:auto_route/auto_route.dart';
+
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  @override
+  RouteType get defaultRouteType => const RouteType.adaptive();
+
+  @override
+  List<AutoRoute> get routes => [];
+}
